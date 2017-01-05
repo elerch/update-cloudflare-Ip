5898e502bad20014010a5863989770a93064e44e
Emil Lerch <emil@lerch.org>
2017-01-05 15:22:03 -0800

    skeleton out files

---
+++ test/update-ip.js
@@ -0,0 +1,12 @@
+'use strict';
+var updateIp = require('../update-ip.js').updateIp;
+var expect = require('chai').expect;
+
+describe('updateIp', () => {
+  it('should update the IP in CloudFlare', () => {
+    expect(true).to.be.false;
+  });
+});
+
+describe('lambda integration', () => {
+});
