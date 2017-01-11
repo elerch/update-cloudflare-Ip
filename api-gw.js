46c37fe901e625d12161c06059c9a2c763a5ec3e
Emil Lerch <emil@lerch.org>
2017-01-11 11:08:54 -0800

    add linting

--- api-gw.js
+++ api-gw.js
@@ -1,47 +1,47 @@
-'use strict';
-var updateIp = require('./update-ip.js');
+// 'use strict';
+const updateIp = require('./update-ip.js');
 
-(function(exports, undefined) {
+/* eslint-disable no-param-reassign,no-console */
+(function main(exports) {
   function clientIpFromXForwardedFor(forwardedFor) {
     return forwardedFor.match(/^[^, ]*/)[0];
   }
-
   exports.clientIpFromXForwardedFor = clientIpFromXForwardedFor;
-  exports.handler =  (event, context, callback) => {
-      console.log(JSON.stringify(event));
-      if (!event.params) {
-        console.log('unknown event received: ' + JSON.stringify(event));
-        callback('unknown event');
+  exports.handler = (event, context, callback) => {
+    console.log(JSON.stringify(event));
+    if (!event.params) {
+      console.log(`unknown event received: ${JSON.stringify(event)}`);
+      callback('unknown event');
+      return;
+    }
+    if (!event.params.path || !event.params.path.host) {
+      callback('API GW misconfigured. Missing expected path var "host"');
+      return;
+    }
+    const host = event.params.path.host;
+    const forwardedFor = event.params.header['X-Forwarded-For'];
+    if (!forwardedFor) {
+      callback('Could not find X-Forwarded-For HTTP Header');
+      return;
+    }
+    const clientIp = clientIpFromXForwardedFor(forwardedFor);
+    if (!clientIp) {
+      callback('Client IP could not be found from X-Forwarded-For ' +
+               `"${forwardedFor}"`);
+      return;
+    }
+    console.log(`X-Forwarded-For: ${forwardedFor}`);
+    console.log(`updating IP of host '${host}' to '${clientIp}'`);
+    updateIp(host, clientIp, (err, data) => {
+      if (err) {
+        console.log(`error updating: ${JSON.stringify(err)}`);
+        callback(err);
         return;
       }
-      if (!event.params.path || !event.params.path.host) {
-        callback('API GW misconfigured. Missing expected path variable "host"');
-        return;
-      }
-      const host = event.params.path.host;
-      const forwardedFor = event.params.header['X-Forwarded-For'];
-      if (!forwardedFor) {
-        callback('Could not find X-Forwarded-For HTTP Header');
-        return;
-      }
-      const clientIp = clientIpFromXForwardedFor(forwardedFor);
-      if (!clientIp) {
-        callback('Client IP could not be found from X-Forwarded-For "' +
-          forwardedFor + '"');
-        return;
-      }
-      console.log("X-Forwarded-For: " + forwardedFor);
-      console.log("updating IP of host '" + host + "' to '" + clientIp + "'");
-      updateIp(host, clientIp, (err, data) => {
-        if (err) {
-          console.log("error updating: " + JSON.stringify(err));
-          callback(err);
-          return;
-        }
-        console.log('completed host update');
-        console.log(data);
-        callback(null, data);
-      });
+      console.log('completed host update');
+      console.log(data);
+      callback(null, data);
+    });
   };
 }(exports));
 
